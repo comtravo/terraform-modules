829c7ffaf8533c39d8bcd0d8e45e049de6cba01a
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2021-12-03 21:43:40 +0100

    nits

--- ecs-service/README.md
+++ ecs-service/README.md
@@ -1,68 +1,44 @@
 ## Requirements
 
-| Name | Version |
-|------|---------|
-| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 0.12 |
+No requirements.
 
 ## Providers
 
 | Name | Version |
 |------|---------|
-| <a name="provider_aws"></a> [aws](#provider\_aws) | n/a |
-
-## Modules
-
-No modules.
-
-## Resources
-
-| Name | Type |
-|------|------|
-| [aws_alb_listener_rule.service](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/alb_listener_rule) | resource |
-| [aws_alb_target_group.service](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/alb_target_group) | resource |
-| [aws_appautoscaling_policy.ecs_policy](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/appautoscaling_policy) | resource |
-| [aws_appautoscaling_target.ecs_target](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/appautoscaling_target) | resource |
-| [aws_ecs_service.daemon](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service) | resource |
-| [aws_ecs_service.service](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service) | resource |
-| [aws_ecs_task_definition.service](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_task_definition) | resource |
-| [aws_iam_role.ecs_lb_role](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role) | resource |
-| [aws_iam_role_policy_attachment.ecs_lb_role](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment) | resource |
-| [aws_route53_record.internal](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record) | resource |
+| aws | n/a |
 
 ## Inputs
 
 | Name | Description | Type | Default | Required |
 |------|-------------|------|---------|:--------:|
-| <a name="input_alb"></a> [alb](#input\_alb) | ECS service ALB configuration | <pre>list(object({<br>    pattern      = string<br>    listener_arn = string<br>  }))</pre> | `[]` | no |
-| <a name="input_alb_listener_count"></a> [alb\_listener\_count](#input\_alb\_listener\_count) | Number of ALB load balancers | `number` | `0` | no |
-| <a name="input_capacity"></a> [capacity](#input\_capacity) | ECS service capacity | <pre>object({<br>    min            = number<br>    max            = number<br>    desired        = number<br>    enable_scaling = bool<br>    target_value   = number<br>  })</pre> | n/a | yes |
-| <a name="input_cluster_id"></a> [cluster\_id](#input\_cluster\_id) | ECS cluster id | `string` | n/a | yes |
-| <a name="input_container_definition"></a> [container\_definition](#input\_container\_definition) | ECS container definition | `string` | n/a | yes |
-| <a name="input_deregistration_delay"></a> [deregistration\_delay](#input\_deregistration\_delay) | Deregistration delay | `number` | `60` | no |
-| <a name="input_ecs_autoscaling_service_linked_role"></a> [ecs\_autoscaling\_service\_linked\_role](#input\_ecs\_autoscaling\_service\_linked\_role) | ECS autoscaling service linked role | `string` | `""` | no |
-| <a name="input_ecs_stability_check_config"></a> [ecs\_stability\_check\_config](#input\_ecs\_stability\_check\_config) | Configuration to wait for a service to be stable | <pre>object({<br>    role     = string<br>    timeout  = number<br>    interval = number<br>  })</pre> | n/a | yes |
-| <a name="input_enable"></a> [enable](#input\_enable) | Enable module | `bool` | `true` | no |
-| <a name="input_environment"></a> [environment](#input\_environment) | Environment | `string` | n/a | yes |
-| <a name="input_execution_role_arn"></a> [execution\_role\_arn](#input\_execution\_role\_arn) | task execution role arn | `string` | `null` | no |
-| <a name="input_force_new_deployment"></a> [force\_new\_deployment](#input\_force\_new\_deployment) | Force new deployment | `bool` | `false` | no |
-| <a name="input_health_check"></a> [health\_check](#input\_health\_check) | n/a | `map(string)` | `{}` | no |
-| <a name="input_load_balancer"></a> [load\_balancer](#input\_load\_balancer) | n/a | `string` | `""` | no |
-| <a name="input_load_balancing_algorithm_type"></a> [load\_balancing\_algorithm\_type](#input\_load\_balancing\_algorithm\_type) | https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group#load_balancing_algorithm_type | `string` | `"least_outstanding_requests"` | no |
-| <a name="input_max_healthy_percent"></a> [max\_healthy\_percent](#input\_max\_healthy\_percent) | Maximum healthy percentage | `number` | `null` | no |
-| <a name="input_min_healthy_percent"></a> [min\_healthy\_percent](#input\_min\_healthy\_percent) | Minimum healthy percentage | `number` | `null` | no |
-| <a name="input_name"></a> [name](#input\_name) | ECS service name | `string` | n/a | yes |
-| <a name="input_network_mode"></a> [network\_mode](#input\_network\_mode) | Service network mode | `string` | `"bridge"` | no |
-| <a name="input_placement_constraints"></a> [placement\_constraints](#input\_placement\_constraints) | ECS service contraints | <pre>list(object({<br>    type       = string<br>    expression = string<br>  }))</pre> | `[]` | no |
-| <a name="input_placement_strategy"></a> [placement\_strategy](#input\_placement\_strategy) | ECS service placement strategy | <pre>list(object({<br>    field = string<br>    type  = string<br>  }))</pre> | <pre>[<br>  {<br>    "field": "attribute:ecs.availability-zone",<br>    "type": "spread"<br>  },<br>  {<br>    "field": "memory",<br>    "type": "binpack"<br>  }<br>]</pre> | no |
-| <a name="input_port_mappings"></a> [port\_mappings](#input\_port\_mappings) | ECS port mappings | <pre>list(object({<br>    hostPort      = number<br>    containerPort = number<br>    protocol      = string<br>  }))</pre> | `[]` | no |
-| <a name="input_region"></a> [region](#input\_region) | AWS region | `string` | n/a | yes |
-| <a name="input_route53"></a> [route53](#input\_route53) | ECS service R53 configuration | <pre>list(object({<br>    zone_id                = string<br>    name                   = string<br>    type                   = string<br>    alias_name             = string<br>    alias_zone_id          = string<br>    evaluate_target_health = bool<br>  }))</pre> | `[]` | no |
-| <a name="input_route53_count"></a> [route53\_count](#input\_route53\_count) | Number of R53 records | `number` | `0` | no |
-| <a name="input_scheduling_strategy"></a> [scheduling\_strategy](#input\_scheduling\_strategy) | n/a | `string` | `"REPLICA"` | no |
-| <a name="input_task_role_arn"></a> [task\_role\_arn](#input\_task\_role\_arn) | task role arn | `string` | n/a | yes |
-| <a name="input_volumes"></a> [volumes](#input\_volumes) | ECS task volumes | <pre>list(object({<br>    name      = string<br>    host_path = string<br>  }))</pre> | `[]` | no |
-| <a name="input_vpc_id"></a> [vpc\_id](#input\_vpc\_id) | VPC id | `string` | n/a | yes |
+| capacity | ECS service capacity | <pre>object({<br>    min            = number<br>    max            = number<br>    desired        = number<br>    enable_scaling = bool<br>    target_value   = number<br>  })</pre> | n/a | yes |
+| cluster\_name | Name of the ECS cluster | `string` | n/a | yes |
+| container\_definitions | ECS container definition | `string` | n/a | yes |
+| deployment\_percent | ECS deployment healthy percentage | <pre>object({<br>    max_healthy_percent = number<br>    min_healthy_percent = number<br>  })</pre> | <pre>{<br>  "max_percent": 100,<br>  "min_healthy_percent": 0<br>}</pre> | no |
+| deregistration\_delay | Deregistration delay | `number` | `60` | no |
+| enable\_ecs\_managed\_tags | Enable ECS managed tags | `bool` | `true` | no |
+| execution\_role\_arn | ECS task execution role arn | `string` | `null` | no |
+| force\_new\_deployment | Force new deployment | `bool` | `false` | no |
+| health\_check | ECS service target group health check configuration | `map(string)` | `{}` | no |
+| launch\_type | ECS service launch type | `string` | `"EC2"` | no |
+| load\_balancer | Target groups to create and attach to the load balancer | <pre>map(object({<br>    listener_arn                  = string<br>    load_balancing_algorithm_type = string<br>    deregistration_delay          = number<br>    container_port                = number<br>    protocol                      = string<br>    health_check = object({<br>      enabled             = bool<br>      healthy_threshold   = number<br>      matcher             = string<br>      interval            = number<br>      path                = string<br>      port                = number<br>      protocol            = string<br>      timeout             = number<br>      unhealthy_threshold = number<br>    })<br>    stickiness = object({<br>      enabled         = bool<br>      type            = string<br>      cookie_duration = number<br>      cookie_name     = string<br>    })<br>    condition_host_header_values  = list(string)<br>    condition_path_pattern_values = list(string)<br>    aws_route53_record = list(object({<br>      zone_id = string<br>      name    = string<br>      type    = string<br>      alias = object({<br>        name                   = string<br>        zone_id                = string<br>        evaluate_target_health = bool<br>      })<br>    }))<br>  }))</pre> | n/a | yes |
+| load\_balancing\_algorithm\_type | https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group#load_balancing_algorithm_type | `string` | `"least_outstanding_requests"` | no |
+| name | Name of the ECS service | `string` | n/a | yes |
+| network\_mode | ECS service network mode | `string` | `"bridge"` | no |
+| placement\_constraints | ECS service placement contraints | <pre>list(object({<br>    type       = string<br>    expression = string<br>  }))</pre> | `[]` | no |
+| placement\_strategy | ECS service placement strategy | <pre>list(object({<br>    field = string<br>    type  = string<br>  }))</pre> | <pre>[<br>  {<br>    "field": "attribute:ecs.availability-zone",<br>    "type": "spread"<br>  },<br>  {<br>    "field": "memory",<br>    "type": "binpack"<br>  }<br>]</pre> | no |
+| propagate\_tags | propogate tags from | `string` | `"SERVICE"` | no |
+| route53 | ECS service R53 configuration | <pre>list(object({<br>    zone_id                = string<br>    name                   = string<br>    type                   = string<br>    alias_name             = string<br>    alias_zone_id          = string<br>    evaluate_target_health = bool<br>  }))</pre> | `[]` | no |
+| scheduling\_strategy | ECS service scheduling strategy | `string` | `"REPLICA"` | no |
+| tags | ECS tags | `map(string)` | `{}` | no |
+| task\_role\_arn | ECS task role arn | `string` | n/a | yes |
+| volumes | ECS task volumes | <pre>list(object({<br>    name      = string<br>    host_path = string<br>  }))</pre> | `[]` | no |
+| vpc\_id | VPC id | `string` | n/a | yes |
+| wait\_for\_steady\_state | Wait for services to be stable | `bool` | `true` | no |
 
 ## Outputs
 
-No outputs.
+| Name | Description |
+|------|-------------|
+| target\_groups | LB target group attributes |
--- ecs-service/main.tf
+++ ecs-service/main.tf
@@ -1,61 +1,10 @@
-locals {
-  default_min_healthy_percent = var.capacity.desired > 1 ? 50 : 0
-  default_max_healthy_percent = 100
-  min_healthy_percent         = var.min_healthy_percent != null ? var.min_healthy_percent : local.default_min_healthy_percent
-  max_healthy_percent         = var.max_healthy_percent != null ? var.max_healthy_percent : local.default_max_healthy_percent
-
-  enable_count         = var.enable ? 1 : 0
-  enable_scaling_count = var.enable == true && var.scheduling_strategy == "REPLICA" && var.capacity.enable_scaling == true ? 1 : 0
-
-  service_count = var.scheduling_strategy == "REPLICA" && var.enable == true ? 1 : 0
-  daemon_count  = var.scheduling_strategy == "DAEMON" && var.enable == true ? 1 : 0
-
-  default_tags = {
-    Name        = "${var.name}-${var.environment}"
-    Environment = var.environment
-  }
-}
-
-resource "aws_alb_target_group" "service" {
-  count                         = local.service_count
-  name                          = "${var.name}-${var.environment}"
-  port                          = var.port_mappings[0]["containerPort"]
-  protocol                      = "HTTP"
-  vpc_id                        = var.vpc_id
-  deregistration_delay          = var.deregistration_delay
-  load_balancing_algorithm_type = var.load_balancing_algorithm_type
-
-  health_check {
-    interval            = lookup(var.health_check, "interval", 30)
-    path                = lookup(var.health_check, "path", "/")
-    healthy_threshold   = lookup(var.health_check, "healthy_threshold", 2)
-    unhealthy_threshold = lookup(var.health_check, "unhealthy_threshold", 8)
-    timeout             = lookup(var.health_check, "timeout", 5)
-    matcher             = lookup(var.health_check, "matcher", 200)
-    port                = "traffic-port"
-  }
-
-  stickiness {
-    type            = "lb_cookie"
-    enabled         = true
-    cookie_duration = 60
-  }
-
-  tags = merge(
-    local.default_tags,
-    {
-      "Loadbalancer" = var.load_balancer
-    },
-  )
-}
-
 resource "aws_ecs_task_definition" "service" {
-  count                 = local.enable_count
-  family                = "${var.name}-${var.environment}"
-  container_definitions = var.container_definition
+  family                = var.name
+  container_definitions = var.container_definitions
   task_role_arn         = var.task_role_arn
   execution_role_arn    = var.execution_role_arn
   network_mode          = var.network_mode
+
   dynamic "volume" {
     for_each = var.volumes
     content {
@@ -64,128 +13,11 @@ resource "aws_ecs_task_definition" "service" {
     }
   }
 
-  tags = local.default_tags
+  tags = var.tags
 }
 
-resource "aws_appautoscaling_target" "ecs_target" {
-  count              = local.enable_scaling_count
-  max_capacity       = var.capacity.max
-  min_capacity       = var.capacity.min
-  resource_id        = "service/${local.cluster_name}/${var.name}"
-  role_arn           = var.ecs_autoscaling_service_linked_role
-  scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
-
-  depends_on = [aws_ecs_service.service]
-}
-
-resource "aws_appautoscaling_policy" "ecs_policy" {
-  count              = local.enable_scaling_count
-  name               = "${local.cluster_name}-${var.name}"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = "service/${local.cluster_name}/${var.name}"
-  scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
-
-  target_tracking_scaling_policy_configuration {
-    target_value       = var.capacity.target_value
-    scale_out_cooldown = 120
-    scale_in_cooldown  = 300
-
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
-    }
-  }
-
-  depends_on = [aws_appautoscaling_target.ecs_target]
-}
-
-resource "aws_ecs_service" "service" {
-  count                              = local.service_count
-  name                               = var.name
-  cluster                            = var.cluster_id
-  task_definition                    = aws_ecs_task_definition.service[0].arn
-  launch_type                        = "EC2"
-  desired_count                      = var.capacity.desired
-  iam_role                           = aws_iam_role.ecs_lb_role[0].arn
-  deployment_maximum_percent         = local.max_healthy_percent
-  deployment_minimum_healthy_percent = local.min_healthy_percent
-  force_new_deployment               = var.force_new_deployment
-  wait_for_steady_state              = true
-  dynamic "ordered_placement_strategy" {
-    for_each = var.placement_strategy
-    content {
-      field = ordered_placement_strategy.value.field
-      type  = ordered_placement_strategy.value.type
-    }
-  }
-  scheduling_strategy = var.scheduling_strategy
-  dynamic "placement_constraints" {
-    for_each = var.placement_constraints
-    content {
-      expression = placement_constraints.value.expression
-      type       = placement_constraints.value.type
-    }
-  }
-
-  load_balancer {
-    target_group_arn = aws_alb_target_group.service[0].arn
-    container_name   = var.name
-    container_port   = var.port_mappings[0]["containerPort"]
-  }
-
-  depends_on = [
-    aws_iam_role.ecs_lb_role,
-    aws_ecs_task_definition.service,
-  ]
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes  = [desired_count]
-  }
-}
-
-resource "aws_ecs_service" "daemon" {
-  count           = local.daemon_count
-  name            = var.name
-  cluster         = var.cluster_id
-  task_definition = aws_ecs_task_definition.service[0].arn
-  launch_type     = "EC2"
-
-  scheduling_strategy = var.scheduling_strategy
-
-  depends_on = [
-    aws_iam_role.ecs_lb_role,
-    aws_ecs_task_definition.service,
-  ]
-
-
-}
-
-resource "aws_alb_listener_rule" "service" {
-  count        = local.enable_count * var.alb_listener_count
-  listener_arn = var.alb[count.index]["listener_arn"]
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_alb_target_group.service[0].arn
-  }
-
-  condition {
-    host_header {
-      values = [
-        var.alb[count.index]["pattern"]
-      ]
-    }
-  }
-
-
-}
-
-resource "aws_iam_role" "ecs_lb_role" {
-  count                 = local.enable_count
-  name                  = "${var.name}-alb-role-${var.environment}"
-  path                  = "/${var.environment}/"
+resource "aws_iam_role" "ecs_service_role" {
+  name                  = "${var.name}-ecs-role"
   force_detach_policies = true
 
   assume_role_policy = <<EOF
@@ -204,22 +36,140 @@ resource "aws_iam_role" "ecs_lb_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_lb_role" {
-  count      = local.enable_count
-  role       = aws_iam_role.ecs_lb_role[0].id
+resource "aws_iam_role_policy_attachment" "ecs_service_role" {
+  role       = aws_iam_role.ecs_service_role.id
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }
 
-resource "aws_route53_record" "internal" {
-  count   = local.enable_count * var.route53_count
-  zone_id = var.route53[count.index]["zone_id"]
-  name    = var.route53[count.index]["name"]
-  type    = "A"
+resource "aws_ecs_service" "service" {
+  name                               = var.name
+  cluster                            = var.cluster_name
+  task_definition                    = aws_ecs_task_definition.service.arn
+  launch_type                        = var.launch_type
+  desired_count                      = var.capacity.desired
+  iam_role                           = aws_iam_role.eecs_service_role.arn
+  deployment_maximum_percent         = var.deployment_percent.max_percent
+  deployment_minimum_healthy_percent = var.deployment_percent.min_healthy_percent
+  force_new_deployment               = var.force_new_deployment
+  scheduling_strategy                = var.scheduling_strategy
+  enable_ecs_managed_tags            = var.enable_ecs_managed_tags
+  propagate_tags                     = var.propagate_tags
+  wait_for_steady_state              = var.wait_for_steady_state
 
-  alias {
-    name                   = var.route53[count.index]["alias_name"]
-    zone_id                = var.route53[count.index]["alias_zone_id"]
-    evaluate_target_health = var.route53[count.index]["evaluate_target_health"]
+  dynamic "ordered_placement_strategy" {
+    for_each = var.placement_strategy
+    content {
+      field = ordered_placement_strategy.value.field
+      type  = ordered_placement_strategy.value.type
+    }
+  }
+
+  dynamic "placement_constraints" {
+    for_each = var.placement_constraints
+    content {
+      expression = placement_constraints.value.expression
+      type       = placement_constraints.value.type
+    }
+  }
+
+  tags = var.tags
+
+  dynamic "load_balancer" {
+    for_each = var.load_balancer != null ? [var.load_balancer] : []
+    content {
+      target_group_arn = aws_lb_target_group.service[load_balancer.key].arn
+      container_name   = var.name
+      container_port   = load_balancer.value.container_port
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [desired_count]
   }
 }
 
+resource "aws_lb_target_group" "service" {
+  for_each = var.load_balancer != null ? [var.load_balancer] : []
+
+  name                          = var.name
+  port                          = each.value.container_port
+  protocol                      = each.value.protocol
+  vpc_id                        = var.vpc_id
+  deregistration_delay          = each.value.deregistration_delay
+  load_balancing_algorithm_type = each.value.load_balancing_algorithm_type
+
+  health_check {
+    enabled             = each.value.health_check.enabled
+    healthy_threshold   = each.value.health_check.healthy_threshold
+    unhealthy_threshold = each.value.health_check.unhealthy_threshold
+    matcher             = each.value.health_check.matcher
+    interval            = each.value.health_check.interval
+    path                = each.value.health_check.path
+    port                = each.value.health_check.port
+    protocol            = each.value.health_check.protocol
+    timeout             = each.value.health_check.timeout
+  }
+
+  dynamic "stickiness" {
+    for_each = each.value.stickiness != null ? [each.value.stickiness] : []
+    content {
+      type            = stickiness.value.type
+      enabled         = stickiness.value.enabled
+      cookie_duration = stickiness.value.cookie_duration
+      cookie_name     = stickiness.value.cookie_name
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "aws_lb_listener_rule" "service" {
+  for_each = aws_lb_target_group.service
+
+  listener_arn = var.load_balancers[each.key].listener_arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.service[each.key].arn
+  }
+
+
+  dynamic "condition" {
+    for_each = each.value.condition_host_header_values != null ? [each.value.condition_host_header_values] : []
+    content {
+      host_header {
+        values = condition.value
+      }
+    }
+  }
+
+  dynamic "condition" {
+    for_each = each.value.condition_path_pattern_values != null ? [each.value.condition_path_pattern_values] : []
+    content {
+      path_pattern {
+        values = condition.value
+      }
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "aws_route53_record" "this" {
+  for_each = toset([for loadbalancer in var.load_balancer : load_balancer.aws_route53_record])
+
+  zone_id = each.value.zone_id
+  name    = each.value.name
+  type    = each.value.type
+
+  alias {
+    name                   = each.value.alias_name
+    zone_id                = each.value.alias_zone_id
+    evaluate_target_health = each.value.evaluate_target_health
+  }
+}
+
+output "target_groups" {
+  value       = aws_lb_target_group.service
+  description = "LB target group attributes"
+}
