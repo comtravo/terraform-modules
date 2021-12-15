fb481c52f75cb3798a0f84757ec0ed3361ae5aaa
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2021-12-15 14:53:54 +0100

    nits

--- ecs-nlb-service/main.tf
+++ ecs-nlb-service/main.tf
@@ -16,31 +16,31 @@ resource "aws_ecs_task_definition" "this" {
   tags = var.tags
 }
 
-resource "aws_iam_role" "ecs_service_role" {
-  name                  = "${var.name}-ecs-service-role"
-  path                  = "/environment/${terraform.workspace}/"
-  force_detach_policies = true
+# resource "aws_iam_role" "ecs_service_role" {
+#   name                  = "${var.name}-ecs-service-role"
+#   path                  = "/environment/${terraform.workspace}/"
+#   force_detach_policies = true
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2008-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": ["ecs.amazonaws.com"]
-      },
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-}
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2008-10-17",
+#   "Statement": [
+#     {
+#       "Action": "sts:AssumeRole",
+#       "Principal": {
+#         "Service": ["ecs.amazonaws.com"]
+#       },
+#       "Effect": "Allow"
+#     }
+#   ]
+# }
+# EOF
+# }
 
-resource "aws_iam_role_policy_attachment" "ecs_service_linked_role" {
-  role       = aws_iam_role.ecs_service_role.id
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
-}
+# resource "aws_iam_role_policy_attachment" "ecs_service_linked_role" {
+#   role       = aws_iam_role.ecs_service_role.id
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+# }
 
 resource "aws_ecs_service" "service" {
   name                               = var.name
@@ -48,7 +48,7 @@ resource "aws_ecs_service" "service" {
   task_definition                    = aws_ecs_task_definition.this.arn
   launch_type                        = var.launch_type
   desired_count                      = var.capacity.desired
-  iam_role                           = aws_iam_role.ecs_service_role.arn
+  # iam_role                           = aws_iam_role.ecs_service_role.arn
   deployment_maximum_percent         = var.deployment_percent.max_percent
   deployment_minimum_healthy_percent = var.deployment_percent.min_healthy_percent
   force_new_deployment               = var.force_new_deployment
