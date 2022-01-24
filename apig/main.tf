ab321043de1bd780afb9e7439ac0f9c14ee50e0b
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2022-01-24 12:33:09 +0100

    Use latest recommendations for API Gateway stages and deployments

--- apig/main.tf
+++ apig/main.tf
@@ -24,21 +24,17 @@ resource "aws_api_gateway_rest_api" "api" {
   body        = var.definition
 }
 
-# Deploy on change
-# https://github.com/hashicorp/terraform/issues/6613
 resource "aws_api_gateway_deployment" "api-deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = ""
 
-  # see bug: https://github.com/terraform-providers/terraform-provider-aws/issues/2918
-
-  variables = {
-    "version" = md5(var.definition)
+  # Deploy on change
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.api.body))
   }
+
   lifecycle {
     create_before_destroy = true
   }
-  depends_on = [aws_api_gateway_rest_api.api]
 }
 
 resource "aws_api_gateway_stage" "stage" {
