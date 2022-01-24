4bf19812ef2c20a9950972f9c370a0b9a152153b
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2022-01-24 16:46:27 +0100

    Prepare moving to monorepo

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/comtravo/terraform-modules
 
 go 1.14
+
+require (
+	github.com/gruntwork-io/terratest v0.38.9
+	github.com/stretchr/testify v1.7.0
+)
--- sfn/examples/lambda/main.tf
+++ sfn/examples/lambda/main.tf
@@ -104,7 +104,7 @@ output "arn" {
 
 module "lambda" {
 
-  source = "github.com/comtravo/terraform-aws-lambda?ref=5.0.0"
+  source = "github.com/comtravo/terraform-aws-lambda?ref=6.4.0"
 
   file_name     = "${path.module}/../../test/fixtures/foo.zip"
   function_name = local.lambda_name
